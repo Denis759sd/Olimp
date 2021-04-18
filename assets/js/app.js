3cb85c26573ad45e7107b5a975a81403d5727f05
Dzianis <kotickpro@gmail.com>
2021-04-18 23:49:30 +0300

    added: app.js , signin.html, signup.html, linked all html file app.js

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+var login_btn = document.getElementById('btn-login-form');
+var register_btn = document.getElementById('btn-register-form');
+
+login_btn.onclick = function(){
+    window.location.href = 'signin.html'
+}
+
+register_btn.onclick = function(){
+    window.location.href = 'signup.html'
+}
+
